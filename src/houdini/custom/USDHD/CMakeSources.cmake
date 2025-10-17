6a965af9aa9276010b755b65b247406032ac4781
SideFX <sidefx@sidefx.com>
2025-10-17 13:11:57 -0400

    Matching to Houdini build 21.0.506.

---
+++ src/houdini/custom/USDHD/CMakeSources.cmake
@@ -0,0 +1,23 @@
+set( sources
+    HD_CameraAdapter.C
+    HD_CoordSysCameraDataSceneIndex.C
+    HD_CoordSysPrimSceneIndexPlugin.C
+    HD_DependencySceneIndexPlugin.C
+    HD_DependencyForwardingSceneIndexPlugin.C
+    HD_DrawModeAdapter.C
+    HD_HoudiniCameraPlateAPIAdapter.C
+    HD_HoudiniFieldAssetAdapter.C
+    HD_GeoConversionSceneIndexPlugin.C
+    HD_HoudiniRendererPlugin.C
+    HD_KarmaAtmosphereAdapter.C
+    HD_KarmaSkyDomeLightAdapter.C
+    HD_HoudiniLightBarnDoorAPIAdapter.C
+    HD_HoudiniViewportGuideAPIAdapter.C
+    HD_LightLinkingSceneIndexPlugin.C
+    HD_MetaCurvesAdapter.C
+    HD_MeshLightSceneIndex.C
+    HD_MeshLightSceneIndexPlugin.C
+    HD_PortalLightSceneIndex.C
+    HD_PortalLightSceneIndexPlugin.C
+)
+
